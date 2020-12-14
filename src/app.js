09822890052ec08cc172e98a4fe7485002bb4781
Martha <mm@marthas-air.attlocal.net>
2020-12-14 15:12:27 -0800

    contact page outlined just need form functionality

--- src/app.js
+++ src/app.js
@@ -16,9 +16,16 @@ hbs.registerPartials(partialPath);
 app.use(express.static(publicDir));
 
 app.get('', (req, res) => {
-  res.send('Hi there!');
+  res.render('index', { title: 'Welcome' });
 });
 
+app.get('/skills', (req, res) => {
+  res.render('skills', { title: 'Skills' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact me' });
+});
 app.listen(4000, () => {
-  console.log('server is up on port 3000');
+  console.log('server is up on port 4000');
 });
